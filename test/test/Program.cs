912ee6d9542e9829c4eaca755099beb1cafd0e0a
stupic0713 <stupic0713@gmail.com>
2020-07-06 18:24:08 +0900

    jjj

---
+++ test/test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Backjoon
+{
+    class main
+    {
+        static void Main(String[] args)
+        {
+            String arg = "aaabbcccb";
+
+            if (arg.Contains("b"))
+                Console.WriteLine("있음");
+        }
+    }
+}
